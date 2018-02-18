a0f35be754c5b11dc5d4685c3fec4502d4cb032f
Alysson A Costa <alysson.avila.costa@gmail.com>
2018-02-18 19:23:54 +0000

    Template model created

---
+++ db/migrate/20180218192232_create_templates.rb
@@ -0,0 +1,12 @@
+class CreateTemplates < ActiveRecord::Migration[5.2]
+  def change
+    create_table :templates do |t|
+      t.string :name
+      t.string :description
+      t.boolean :is_active
+      t.integer :position
+
+      t.timestamps
+    end
+  end
+end
