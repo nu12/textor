30ce937d02b4f3b615e9bf0832f4fe02185def22
Alysson A Costa <alysson.avila.costa@gmail.com>
2018-02-21 00:04:20 +0000

    Added validation to name and description

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,2 +1,5 @@
 class Template < ApplicationRecord
+	validates :name, presence: { message: "Template name is missing." }
+	validates :name, uniqueness: { message: "This Template name is already taken." }
+	validates :description, presence: { message: "Template description is missing." }
 end
