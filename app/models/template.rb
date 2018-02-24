270c525e18e53f6a6198d20a998537cb07a6136c
Alysson A Costa <alysson.avila.costa@gmail.com>
2018-02-24 18:36:15 +0000

    Added list behaviour to Template

--- app/models/template.rb
+++ app/models/template.rb
@@ -1,9 +1,12 @@
 class Template < ApplicationRecord
-	has_many :papers
-	has_many :template_sections
-	has_many :sections, through: :template_sections
-
-	validates :name, presence: { message: "Template name is missing." }
-	validates :name, uniqueness: { message: "This Template name is already taken." }
-	validates :description, presence: { message: "Template description is missing." }
+  has_many :papers
+  has_many :template_sections
+  has_many :sections, through: :template_sections
+  default_scope { order(position: :asc) }
+  
+  acts_as_list
+  
+  validates :name, presence: { message: "Template name is missing." }
+  validates :name, uniqueness: { message: "This Template name is already taken." }
+  validates :description, presence: { message: "Template description is missing." }
 end
